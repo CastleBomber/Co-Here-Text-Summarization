451e801948075913b8ad9da4ee8a74bc605ded70
cBombs <richie.ucsb@gmail.com>
2022-10-16 07:26:17 -0700

    day 2

--- Read-Text-Goose.py
+++ Read-Text-Goose.py
@@ -14,19 +14,17 @@
 import newspaper
 from newspaper import fulltext
 import requests
+import goose3
+from goose3 import Goose
+from requests import get
 
 def main():
+    response = get(
+        'https://www.bbc.com/news/world-asia-china-63226230')
+    extractor = Goose()
+    article = extractor.extract(raw_html=response.content)
+    text = article.cleaned_text
 
-    #article = newspaper.Article("https://www.bbc.com/news/world-asia-china-63226230")
-    #article.download()
-    #article.html
-    #article.parse()
-    #article.authors
-    #text = fulltext(article)
-    #print(text)
-
-    html = requests.get("https://www.bbc.com/news/world-asia-china-63226230").text
-    text = fulltext(html)
     print(text)
 
 main()
--- Text-Summarization.py
+++ Text-Summarization.py
@@ -11,19 +11,26 @@
 **************************************************************
 """
 
+import newspaper
+from newspaper import fulltext
+import requests
 import cohere
 
 def main():
 
     co = cohere.Client("MN0jDAS3JRs46BEZ9ZYm0MyqISSYMlZlATe62XBA")
+    html = requests.get("https://www.bbc.com/news/world-asia-china-63226230").text
+    article_text = fulltext(html)
 
     response = co.generate(
-        model='large',
-        prompt="Apple are so damn tasty, they should be illegal.",
+        model='xlarge',
+        prompt=article_text,
         max_tokens=40,
         temperature=0.8,
         stop_sequences=["--"])
 
+    print(article_text)
+    print('\n')
     print('Prediction: {}'.format(response.generations[0].text))
 
 
